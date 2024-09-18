37c94e74d3e79566281f24cf9fd7d6f6bc5a516d
Matias Valdez <matiasvaldez33@gmail.com>
2024-09-18 16:17:53 -0300

    Home y Services

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,81 @@
 import React from "react";
-import "../index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Carousel from 'react-bootstrap/Carousel'; //instalar npm install react-bootstrap bootstrap
+
+import '../index.css';
 
 function Home() {
-  return <h1>Bienvenidos a Habita Inmobiliaria</h1>;
+  return (
+    <div>     
+
+      <h1>   </h1>;
+
+      <Carousel fade interval={3000} controls={false} indicators={false} pause="false">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={"src/assets/images/inicio/img1.png"}
+            alt="Primera imagen"
+          />
+          <Carousel.Caption>
+            
+              <p className="small-text">Tu próximo hogar está acá</p>
+              <p > </p>
+              <h2 className="large-text">Tu confianza,<br /> nuestra prioridad</h2>
+              <p > </p>
+              <a href="https://wa.me/message/GABFF33RIWHXJ1" target="_blank" rel="noopener noreferrer">
+                <p className="button-text">Contactá con un asesor</p>
+              </a>
+            
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={"src/assets/images/inicio/img2.png"}
+            alt="Segunda imagen"
+          />
+          <Carousel.Caption>
+            
+              <p className="small-text">Tu próximo hogar está acá</p>
+              <p > </p>
+              <h2 className="large-text">Encontrá el lugar <br /> que mejor va con vos</h2>
+              <p > </p>
+              <a href="https://wa.me/message/GABFF33RIWHXJ1" target="_blank" rel="noopener noreferrer">
+                <p className="button-text">Contactá con un asesor</p>
+              </a>
+            
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={"src/assets/images/inicio/img3.png"}
+            alt="Tercera imagen"
+          />
+          <Carousel.Caption>
+            
+              <p className="small-text">Tu próximo hogar está acá</p>
+              <p > </p>
+              <h2 className="large-text">
+                Nosotros administramos,<br />
+                vos administrá tu tiempo
+              </h2>
+              <p > </p>
+              <a href="https://wa.me/message/GABFF33RIWHXJ1" target="_blank" rel="noopener noreferrer">
+                <p className="button-text">Contactá con un asesor</p>
+              </a>
+            
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      <h1>   </h1>;
+
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,37 @@ import React from "react";
 import "../index.css";
 
 function Services() {
-  return <h1>Servicios</h1>;
+  return (
+    <div>
+      <p>  </p>
+      <p className="service-text">Nuestros Servicios</p>
+      <p>  </p>
+
+      <div className="image-container">
+        <img
+          src={"src/assets/images/servicios/alquiler.png"}
+          alt="Primera imagen"
+        />
+        <img
+          src={"src/assets/images/servicios/administracion.png"}
+          alt="Segunda imagen"
+        />
+        <img
+          src={"src/assets/images/servicios/venta.png"}
+          alt="Tercera imagen"
+        />
+        <img
+          src={"src/assets/images/servicios/tasaciones.png"}
+          alt="Cuarta imagen"
+        />
+        <img
+          src={"src/assets/images/servicios/obra.png"}
+          alt="Quinta imagen"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Services;
+
